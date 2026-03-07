9bd26042e83509a3cba273e5327324435a15b5a0
Anthuan Vasquez <me@anthuanvasquez.net>
2026-03-06 23:51:12 -0400

    style: fix formatting issues and update readme

--- app/components/layouts/Header.vue
+++ app/components/layouts/Header.vue
@@ -97,9 +97,9 @@ onMounted(() => {
                 >
                   {{ item.name }}
                 </a>
-                
+
                 <div class="ml-2 h-4 w-px bg-white/10"></div>
-                
+
                 <ThemeToggle />
               </div>
             </nav>
@@ -138,9 +138,11 @@ onMounted(() => {
         >
           <div class="flex items-center justify-between">
             <a href="/" class="-m-1.5 p-1.5">
-              <span class="text-primary font-firacode text-xl font-bold">{{ '<av />' }}</span>
+              <span class="text-primary font-firacode text-xl font-bold"
+                >{{ '<av />' }}</span
+              >
             </a>
-            
+
             <div class="flex items-center gap-x-3">
               <ThemeToggle />
               <button
